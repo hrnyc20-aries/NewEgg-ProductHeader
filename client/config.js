e0cc2d22301bfca6570fdd840846cd61e7544c68
bradleymorgan05 <bradleymorgan05@gmail.com>
2019-03-09 15:41:08 -0500

    changed AWS IP for service

--- client/config.js
+++ client/config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	// aws: "ec2-18-223-158-147.us-east-2.compute.amazonaws.com"
-	aws: 'ec2-184-72-197-189.compute-1.amazonaws.com'
+  // aws: 'ec2-184-72-197-189.compute-1.amazonaws.com'
+  aes: 'ec2-52-201-197-163.compute-1.amazonaws.com'
 	// aws: "localhost:3010"
 };
